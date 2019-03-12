7cf17a29634f957ce6e52d25be62a2899f176d3a
David Ortega <284179+dorcu@users.noreply.github.com>
2019-03-12 10:24:32 +0100

    Update hola_mundo.ino

--- hola_mundo.ino
+++ hola_mundo.ino
@@ -1,6 +1,6 @@
 // Codigo simple para hacer un Hola Mundo
-// David Ortega Cuadrado - dorcu.com
-// 1 de Abril de 2014
+// David Ortega Cuadrado - BitPirata.com
+// 12 de marzo de 2019
 // dorcu.com
 
 int led = 13; // Creamos una variable de tipo entero (integer) con el valor 13
